e2ff4ee3971b9a660b5a217e58e1775fe9d875c3
Alexandr <yaborodinov@gmail.com>
2020-08-18 17:09:39 +0300

    create section cover

--- src/App/Header/NavMenuAside/NavMenuAside.js
+++ src/App/Header/NavMenuAside/NavMenuAside.js
@@ -5,8 +5,8 @@ const NavMenuAside = () => {
     return (
         <div className="col  nav-menu-aside">
             <ul className="row nav-menu-aside__list">
-                <li className="col nav__item"><a href="">subscribe</a></li>
-                <li className="col nav__item"><a href="">newsletter</a></li>
+                <li className=" nav__item"><a href="">subscribe</a></li>
+                <li className=" nav__item"><a href="">newsletter</a></li>
             </ul>
 
             
--- src/App/Main/Main.js
+++ src/App/Main/Main.js
@@ -4,34 +4,17 @@ import "./Main.css"
 
 const Main = () => {
     return (
-        <section class="section">
+        <section className="section section__cover">
             <div className="container">
-                <div >приветосики!</div>
-                <a href="#">ссылочка</a>
-                <div>loremne 14:48:  The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-
-                ./src/App/Hu/NavMobileMenu.js
-                Line 7:13:  The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-
-                ./src/App/Header/Logo/Logo.js
-                Compiled with warnings.
-
-                ./src/App/Main/Main.js
-                Line 10:17:  The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-NavMainList.js
-                Line 10:48:  The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-                Line 11:48:  The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-                Line 12:48:  The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-                Line 13:48:  The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-                Line 14:48:  The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-
-                ./src/App/Header/NavMnu.js
-                Line 7:13:  The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-
-                ./src/App/Header/Logo/Logo.js
-  Line 8:13:  The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid</div>
-
+                <div className=" secton__cover-row">
+                    <a href="#" ><img className=" section__cover-img" src="images/photo/1.jpg" alt="" /></a>
+                    <div className=" section__cover-title-block">
+                        <h1 className="section__cover-title"><a href="#">Maro Itoje's Trainer Shares the Rugby Player's Explosive Strength Workout</a></h1>
+                        <h2 className="section__cover-subtitle">This is the workout the thinking man’s battering ram uses to build strength</h2>
+                    </div>
+                </div>
             </div>
+            
         </section>
     )
 }
