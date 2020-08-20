65e12f6fb99da89846ef0b293c108247ecb96d22
Alexandr <yaborodinov@gmail.com>
2020-08-20 10:45:13 +0300

    add header__block for main cover in magazin

--- src/App/Header/Header.js
+++ src/App/Header/Header.js
@@ -3,6 +3,7 @@ import Logo from "./Logo/Logo"
 import NavMobileMenu from "./NavMobileMenu/NavMobileMenu"
 import NavMenuList from "./NavMainList/NavMainList"
 import NavMenuAside from "./NavMenuAside/NavMenuAside"
+import MainCover from "./mainCover/MainCover"
 import Search from "./Search/Search"
 
 import "./Header.css"
@@ -10,25 +11,25 @@ import "./Header.css"
 
 const Header = ()=> {
     return (
-        <header className="header">
-            <div className="container">
-                
-                <div className="row header__row">
-                    <NavMobileMenu/> 
-                    <Logo />
-                    <NavMenuList />
-                    <NavMenuAside />
-                    <Search />
-
-                </div>
-
-
-                
-
-
-
-            </div>
+        <>
+        <header className="header__block">
+            <MainCover />
         </header>
+            <div className="header">
+                <div className="container">
+
+                    <div className="row header__row">
+                        <NavMobileMenu/> 
+                        <Logo />
+                        <NavMenuList />
+                        <NavMenuAside />
+                        <Search />
+
+                    </div>
+                </div>
+            </div>
+            </>
+        
     )
 }
 export default Header
---
+++ src/App/Header/mainCover/MainCover.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import "./MainCover.css"
+
+
+const MainCover = () => {
+    return (
+        <section className="section__mainCover">
+            <a href=""><img src="images/photo/2.jpg" alt="" /></a>
+                <div className="mainCover__info col-xs-12 col-sm-8"></div>
+        </section>
+    )
+}
+export default MainCover
