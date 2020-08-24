5c8a9a7d648cf80c7b278f643b2ecdcf35a71250
Alexandr <yaborodinov@gmail.com>
2020-08-24 17:21:04 +0300

    fixed header padding

--- src/App/Header/Header.js
+++ src/App/Header/Header.js
@@ -18,7 +18,7 @@ const Header = ()=> {
             <div className="header">
                 <div className="container">
 
-                    <div className=" header__row">
+                    <div className="row header__row">
                         <NavMobileMenu/> 
                         <Logo />
                         <NavMenuList />
